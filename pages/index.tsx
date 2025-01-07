03a0e8c566773b77e70e0a3064df66965c9500df
Cotori-d_hirama <106217321+cotori-dhirama@users.noreply.github.com>
2025-01-07 17:50:01 +0900

    Change サンプル打ち込み

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,49 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
+import styles from "./index.module.css";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-);
+const IndexPage: NextPage = () => {
+    const [imageUrl, setImageUrl] = useState("");
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchImage().then(newImage => {
+            setImageUrl(newImage.url);
+            setLoading(false);
+        });
+    }, []);
+
+    // ボタン押下で画像読み込み
+    const handleClick = async () => {
+        setLoading(true);   // 読み込み中とする
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url); // 画像URLを更新
+        setLoading(false);  // 読み込み済みとする
+    };
+
+    return (
+        <div className={styles.page}>
+            <button onClick={handleClick} className={styles.button}>ほかのにゃんこも見る</button>
+            <div className={styles.frame}>
+                {loading || <img src={imageUrl} className={styles.img}/>}
+            </div>
+        </div>
+    );
+}
 
 export default IndexPage;
+
+type Image = {
+    url: string;
+};
+
+const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    console.log(images);
+    return images[0];
+}
+
+fetchImage().then(image => {
+    console.log(image.url);
+});
